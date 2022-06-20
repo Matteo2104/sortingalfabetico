03e92d5978e6d017c30845a83ce8750a7d9f4a3e
Matteo2104 <scarcella@outlook.it>
2022-06-20 07:23:05 +0200

    primo commit

---
+++ src/sortingalfabetico/Sorting.java
@@ -0,0 +1,16 @@
+package sortingalfabetico;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sorting {
+	
+	public static List<String> sort(List<String> names) {
+		// converto la lista in input in array di stringhe
+		String[] partialVector = names.toArray(new String[0]);
+
+		Arrays.sort(partialVector);
+		
+		return Arrays.asList(partialVector);
+	}
+}
